299e6cc3e2e7731b9096a679915a61b786d5edff
lujain <lujain.com>
2024-05-06 18:32:47 +0200

    The basics of async

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""modules"""
+import asyncio
+import random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """
+     An asynchronous coroutine that spawns wait_random n times with the
+     specified max_delay and returns a list of all the delays in
+    """
+
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+
+    for i in range(len(delays)):
+        for j in range(i + 1, len(delays)):
+            if delays[i] > delays[j]:
+                delays[i], delays[j] = delays[j], delays[i]
+
+    return delays
