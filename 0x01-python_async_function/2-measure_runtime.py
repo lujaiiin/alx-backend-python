623238f112c0c3c0de215bfefa5742017455c344
lujain <lujain.com>
2024-05-06 21:41:20 +0200

    The basics of async

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,10 +9,10 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     """measure time"""
-  
+
     timez = time()
     run(wait_n(n, max_delay))
     timeo = time()
     elapsed = timeo - timez
-  
+
     return elapsed / n
