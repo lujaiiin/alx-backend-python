0320506cc1a23226d10f589c08213cf649434ef3
lujaiiin <132622956+lujaiiin@users.noreply.github.com>
2024-05-06 21:25:45 +0200

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
 """modules"""
+
 import asyncio
-import random
-
-
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
-    """
-    An asynchronous coroutine that spawns wait_random n times with
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """wait n fun"""
+    spawn_ls = []
+    delays = []
+    for _ in range(n):
+        delayed_task = asyncio.create_task(wait_random(max_delay))
+        delayed_task.add_done_callback(lambda x: delays.append(x.result()))
+        spawn_ls.append(delayed_task)
 
-    Parameters:
-    - n (int): Number of times to spawn wait_random.
-    - max_delay (int): Maximum delay in seconds for each wait_random instance.
-
-    Returns:
-    - list: A list of delays in ascending order.
-    """
-
-    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-
-    for i in range(len(delays)):
-        for j in range(i + 1, len(delays)):
-            if delays[i] > delays[j]:
-                delays[i], delays[j] = delays[j], delays[i]
+    for spawn in spawn_ls:
+        await spawn
 
     return delays
