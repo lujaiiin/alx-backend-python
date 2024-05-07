91b965e1d84ae1ed3b17291f3eb674eec4f9fa3b
lujain <lujain.com>
2024-05-07 23:01:33 +0200

    Async Generator

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""modules"""
+
+import asyncio
+from time import perf_counter
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ function measure"""
+
+    be = perf_counter()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    return perf_counter() - be
